6642f390f537c6324a799ab8654fb28f5bba8ddd
Ayush Abhinandan <ayushabhinandan269@gmail.com>
2026-01-31 15:37:14 +0530

    latest implmentation

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,12 @@ import manualCorrectionRoutes from "./routes/manualCorrection.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: "*",
+}));
+
 app.use(express.json());
 app.use("/api/uploads", uploadRoutes);
 app.use("/api/dashboard", dashboardRoutes);
